7b9f935e77215586722ea7e5d490e556b56636ea
lingpeng <13996598949@163.com>
2018-04-23 19:16:56 +0800

    first

---
+++ src/main/java/com/lp/uav_weixin_back_project/UavWeixinBackProjectApplication.java
@@ -0,0 +1,12 @@
+package com.lp.uav_weixin_back_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UavWeixinBackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UavWeixinBackProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lp/uav_weixin_back_project/UavWeixinBackProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lp.uav_weixin_back_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UavWeixinBackProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
